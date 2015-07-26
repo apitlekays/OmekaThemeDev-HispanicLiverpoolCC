54210836eb7f4720d9580947b4b7829bcb991225
HafizHanif <apitlekays@gmail.com>
2015-07-26 13:56:35 +0100

    Re-configure page layout for uniformity

--- plugins/GuestUser/views/public/user/register.php
+++ plugins/GuestUser/views/public/user/register.php
@@ -6,15 +6,18 @@ queue_css_string($css);
 $pageTitle = get_option('guest_user_register_text') ? get_option('guest_user_register_text') : __('Register');
 echo head(array('bodyclass' => 'register', 'title' => $pageTitle));
 ?>
-<h1><?php echo $pageTitle; ?></h1>
+
 <div id='primary'>
+<h1><?php echo $pageTitle; ?></h1>
 <div id='capabilities'>
 <p>
 <?php echo get_option('guest_user_capabilities'); ?>
 </p>
 </div>
+<div class="register_form">
 <?php echo flash(); ?>
 <?php echo $this->form; ?>
+</div>
 <p id='confirm'></p>
 </div>
 <?php echo foot(); ?>
