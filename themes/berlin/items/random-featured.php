49ab580c67a333d55722781f96b3e83eb88e3c9b
HafizHanif <apitlekays@gmail.com>
2015-07-26 18:44:40 +0100

    edited for displaying random featured items

--- themes/berlin/items/random-featured.php
+++ themes/berlin/items/random-featured.php
@@ -4,17 +4,19 @@
         $title = metadata($item, array('Dublin Core', 'Title'));
         $description = metadata($item, array('Dublin Core', 'Description'), array('snippet' => 150));
         ?>
-        <?php if (metadata($item, 'has thumbnail')) {
+		<p class="item-title">
+	        <?php echo link_to($item, 'show', strip_formatting($title)); ?>
+        </p>
+        <div class="img-base">
+	        <?php if (metadata($item, 'has thumbnail')) {
             echo link_to_item(
                 item_image('square_thumbnail', array(), 0, $item), 
                 array('class' => 'image'), 'show', $item
             );
         }
         ?>
-        <h3><?php echo link_to($item, 'show', strip_formatting($title)); ?></h3>
-        <?php if ($description): ?>
-            <p class="item-description"><?php echo $description; ?></p>
-        <?php endif; ?>
+        </div>
+      </div>
     <?php endforeach; ?>
 <?php else: ?>
     <p><?php echo __('No featured items are available.'); ?></p>
