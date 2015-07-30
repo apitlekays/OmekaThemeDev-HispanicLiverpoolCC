57e887cf6c7015555bc7ca4e2e124c901e59871a
HafizHanif <apitlekays@gmail.com>
2015-07-30 22:42:10 +0100

    Added custom hack for search box

--- themes/omekahispanicliverpool/common/footer.php
+++ themes/omekahispanicliverpool/common/footer.php
@@ -6,11 +6,19 @@
     <div class="disclaimer-container clearfix">
       <a href="http://omeka.org/"><div class="omeka-logo"></div></a>
       <div class="disclaimer">
-        <p>Disclaimer text&nbsp;</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor</p>
+        <p>The Hispanic Liverpool Community Collection Privacy Policy</p>
+        <p>The privacy of your personal information is important to the Hispanic Liverpool Community Collection (referred to henceforth as HLCC). We neither capture nor store personal information through the HLCC without the prior consent of users. Any personal information collected as part of the user registration process or the submission of material (including, but not limited to, name, address, e-mail address, phone number) will be stored securely on Warwick University servers, and accessible only to members of the HLCC project team. We will not sell, license or trade your personal information to others. We do not provide your personal information to direct marketing companies or other such organizations.</p>
 </div>
     </div>
   </footer>
 <!-- End of Footer -->
+<script type="text/javascript">
+	$(function(){
+		$('#baseUrl').click (function(){
+			window.location = $(this).attr('href') + $('#appendUrl').val();
+			return false;
+		});
+	});
+</script>
 </body>
 </html>
